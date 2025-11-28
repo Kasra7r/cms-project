caa8befb30adf2381ec8eb67dd09518252164702
Kasra <kasrarostami70@gmail.com>
2025-11-28 20:25:25 +0330

    Update Conversation.js
    Clean up.

--- cms-backend/models/Conversation.js
+++ cms-backend/models/Conversation.js
@@ -1,17 +1,31 @@
-// models/Conversation.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const conversationSchema = new mongoose.Schema(
   {
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true }],
+    participants: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true }
+    ],
     title: String,
-    isGroup: { type: Boolean, default: false },
-    lastMessageAt: Date,
+    isGroup: { type: Boolean, default: false }
   },
-  { timestamps: true, versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
-conversationSchema.virtual('id').get(function () { return this._id.toHexString(); });
-conversationSchema.set('toJSON', { virtuals: true, transform: (_, ret) => { delete ret._id; return ret; } });
+conversationSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
 
-module.exports = mongoose.model('Conversation', conversationSchema);
+conversationSchema.set("toJSON", {
+  virtuals: true,
+  transform: (_, ret) => {
+    delete ret._id;
+    return ret;
+  }
+});
+
+module.exports = mongoose.model("Conversation", conversationSchema);
