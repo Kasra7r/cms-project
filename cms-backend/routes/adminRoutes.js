ef692c1696c090f2e8916f346e8605a51dc378c7
Kasra <kasrarostami70@gmail.com>
2025-11-28 20:31:42 +0330

    Update adminRoutes.js
    Clean up.

--- cms-backend/routes/adminRoutes.js
+++ cms-backend/routes/adminRoutes.js
@@ -1,16 +1,13 @@
-// routes/adminRoutes.js
-const express = require('express');
-const { checkRole } = require('../middleware/authMiddleware');
+const express = require("express");
+const { checkRole } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post('/create', checkRole(['admin', 'manager']), async (req, res) => {
-  // انجام عملیات ایجاد
-  res.status(201).json({ message: 'Created successfully' });
+router.post("/create", checkRole(["admin", "manager"]), async (req, res) => {
+  res.status(201).json({ message: "Created successfully" });
 });
 
-router.put('/update', checkRole(['admin', 'manager']), async (req, res) => {
-  // انجام عملیات ویرایش
-  res.status(200).json({ message: 'Updated successfully' });
+router.put("/update", checkRole(["admin", "manager"]), async (req, res) => {
+  res.status(200).json({ message: "Updated successfully" });
 });
 
 module.exports = router;
