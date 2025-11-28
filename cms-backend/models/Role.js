e7e527c0f2ae32dd7c0ddb943cc9709f0b531a6a
Kasra <kasrarostami70@gmail.com>
2025-11-28 20:30:31 +0330

    Update Role.js
    Clean up.

--- cms-backend/models/Role.js
+++ cms-backend/models/Role.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   permissions: [
-    { type: String, enum: ['create', 'read', 'update', 'delete'], required: true }
-  ], // دسترسی‌هایی که هر نقش دارد (مثلاً "create", "read")
+    {
+      type: String,
+      enum: ["create", "read", "update", "delete"],
+      required: true
+    }
+  ]
 });
 
-module.exports = mongoose.model('Role', roleSchema);
+module.exports = mongoose.model("Role", roleSchema);
